a5d2e72cde454d58c42df643a24bbdb21a36481a
Najma <najmathande@gmail.com>
2023-07-02 16:11:19 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+# sessions_controller.rb
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+end
+
+# users_controller.rb
+Rails.application.routes.draw do
+  get '/me', to: 'users#show'
 end
